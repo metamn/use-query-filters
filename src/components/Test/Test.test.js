2b06395f49b24e24fe1cf9fe4a63f85d43de2b06
metamn <bartus.csongor@gmail.com>
2019-12-06 10:34:09 +0200

    Test component removed

--- src/components/Test/Test.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Test from "./Test";
-
-it("has a Test component", () => {
-  const { getByText } = render(<Test />);
-  expect(getByText("Test")).toBeInTheDocument();
-});
--- src/components/Test/index.js
+++
@@ -1,1 +0,0 @@
-export { default, TestPropTypes, TestDefaultProps } from "./Test";
