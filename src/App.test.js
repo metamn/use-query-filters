3fc0964523f270a135aea2d81ec6607f2760a489
metamn <bartus.csongor@gmail.com>
2019-12-12 15:45:25 +0200

    Working on requirements

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Requirements", function() {
+  it("Describes the filters via JSON", () => {
+    expect(true);
+  });
+
+  it("Auto generates the input fields using a theme", () => {
+    expect(true);
+  });
+
+  it("Does validation and error messaging", () => {
+    expect(true);
+  });
+
+  it("Filters can be grouped and nested", () => {
+    expect(true);
+  });
+});
